f5e74d9a9c2ce89f10dad218a57aad931b605aac
Youngs285 <nitronite500@gmail.com>
2014-05-01 09:01:10 -0400

    Warmup3

---
+++ WarmUp3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WarmUp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 42; i <= 96; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
